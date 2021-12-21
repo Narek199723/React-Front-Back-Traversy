898c983de6aa4ab76341fa348aa0768879a1c0db
mihranbluecap <mihran@blucap.ai>
2021-12-21 17:48:33 +0400

    Starting GithubFinder

---
+++ working-directory/src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import { Fragment } from "react";
+import UserResults from "../components/users/UserResults";
+
+const Home = () => {
+    return (
+        <Fragment>
+            <UserResults />
+        </Fragment>
+    );
+};
+
+export default Home;
