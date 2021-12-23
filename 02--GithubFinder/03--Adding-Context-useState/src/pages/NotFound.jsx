3fb2e148398420baedece00747640107b40b5fc6
Narek Boshyan <Narek.boshyan@gmail.com>
2021-12-23 17:02:30 +0400

    dasdas

--- 02--GithubFinder/03--Adding-Context-useState/src/pages/NotFound.jsx
+++ 02--GithubFinder/03--Adding-Context-useState/src/pages/NotFound.jsx
@@ -2,7 +2,7 @@ import { FaHome } from "react-icons/fa";
 import { Link } from "react-router-dom";
 
 const NotFound = () => {
-    return (
+    return (   
         <div className="hero">
             <div className="text-center hero-content">
                 <div className="max-w-lg">
