8cd137fb7fc780e052dc5fdc2e251f3dcef75e09
Narek Boshyan <Narek.boshyan@gmail.com>
2021-12-24 18:47:59 +0400

    Firebase

---
+++ 04--House-Market/Firebase-displaydata/src/App.js
@@ -0,0 +1,43 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import Navbar from "./components/Navbar";
+import Explore from "./pages/Explore";
+import ForgotPassword from "./pages/ForgotPassword";
+import Offers from "./pages/Offers";
+import SignIn from "./pages/SignIn";
+import Signup from "./pages/Signup";
+import Profile from "./pages/Profile";
+import { Fragment } from "react";
+import PrivateRoute from "./components/PrivateRoute";
+import Category from "./pages/Category";
+
+const App = () => {
+    return (
+        <Fragment>
+            <Router>
+                <Routes>
+                    <Route path="/" element={<Explore />} />
+                    <Route path="/offers" element={<Offers />} />
+                    <Route
+                        path="/category/:categoryName"
+                        element={<Category />}
+                    />
+                    <Route path="/profile" element={<PrivateRoute />}>
+                        <Route path="/profile" element={<Profile />} />
+                    </Route>
+                    <Route path="/sign-in" element={<SignIn />} />
+                    <Route path="/sign-up" element={<Signup />} />
+                    <Route
+                        path="/forgot-password"
+                        element={<ForgotPassword />}
+                    />
+                </Routes>
+                <Navbar />
+            </Router>
+            <ToastContainer />
+        </Fragment>
+    );
+};
+
+export default App;
